03ecb1d7960148bd7bb7dac8fbc1a1cb5f328522
sjc2004 <118549804+sjc2004@users.noreply.github.com>
2023-11-02 19:53:40 +0530

    Create L1.c

---
+++ L1.c
@@ -0,0 +1,37 @@
+// C program to implement
+// the above approach
+#include<sys/types.h>
+#include<stdio.h>
+// Driver code
+
+int main()
+{
+
+  int pid;
+
+  pid = fork();
+ 
+
+  if (pid == 0)
+
+  {
+
+    printf ("Child : I am the child process\n");
+
+    printf ("Child : Child’s PID: %d\n", getpid());
+
+    printf ("Child : Parent’s PID: %d\n", getppid());
+
+  }
+
+  else
+
+  {
+   printf ("Parent : I am the parent process\n");
+
+    printf ("Parent : Parent’s PID: %d\n", getpid());
+
+    printf ("Parent : Child’s PID: %d\n", pid);
+
+  }
+}
